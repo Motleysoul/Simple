b9a7477ac41b0d74dfaf675a7c776a7abcb4594c
Anna Suvorova <njl@mail.ru>
2023-12-06 18:41:06 +0300

    feat: add mobile menu

--- dist/js/index.bundle.js
+++ dist/js/index.bundle.js
@@ -1,1 +1,1 @@
-new Swiper(".swiper",{loop:!0,mousewheel:!0});
+(()=>{const e=document.querySelector(".header__nav");document.querySelector(".header__mobile-btn").addEventListener("click",(function(){e.classList.contains("header__nav--close")?(e.classList.remove("header__nav--close"),e.classList.add("header__nav--open")):(e.classList.remove("header__nav--open"),e.classList.add("header__nav--close"))})),new Swiper(".swiper",{loop:!0,mousewheel:!0})})();
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
+const indexNav = document.querySelector('.header__nav');
+const navBtn = document.querySelector('.header__mobile-btn');
+
+navBtn.addEventListener('click', function() {
+  if (indexNav.classList.contains('header__nav--close')) {
+    indexNav.classList.remove('header__nav--close');
+    indexNav.classList.add('header__nav--open');
+  } else {
+    indexNav.classList.remove('header__nav--open');
+    indexNav.classList.add('header__nav--close');
+  }
+});
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     mousewheel: true,
